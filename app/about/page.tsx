00940dc272ea674876a8fed3854757e445f17248
Odin Beuchat <odin.beuchat@divtec.ch>
2022-12-29 22:49:11 +0100

    created the first few pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,32 @@
 const About = () => {
 	return (
 		<>
+			<p>
+				Un furry est une personne qui s'intéresse à la culture furry,
+				qui est un mouvement culturel basé autour de la représentation
+				anthropomorphe des animaux dans la littérature, l'art et les médias
+				de divertissement. Les furries sont souvent passionnés par les
+				personnages anthropomorphes dans les dessins animés, les films,
+				les mangas et les jeux vidéo et aiment se représenter eux-mêmes
+				sous la forme d'animaux anthropomorphes,
+				connus sous le nom de "fursonas".
+			</p>
+
+			<p>
+				La culture furry inclut également une communauté en ligne
+				active et des événements organisés, comme des conventions et des
+				meet-ups, où les furries peuvent se rencontrer,
+				échanger et partager leur passion pour la culture furry.
+			</p>
+
+			<p>
+				Il est important de noter que la culture furry est souvent mal
+				comprise et stigmatisée par le grand public, mais il s'agit en
+				réalité d'un mouvement culturel diversifié et inclusif qui regroupe
+				des personnes de tous horizons et de tous âges.
+				Les furries sont simplement des gens passionnés par les animaux
+				anthropomorphes et la culture qui leur est associée.
+			</p>
 		</>
 	);
 };
