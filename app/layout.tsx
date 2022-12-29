b94cbcf4f29bbfe0b4939718b455a3e179cb9dad
Odin Beuchat <odin.beuchat@divtec.ch>
2022-12-29 11:58:47 +0100

    header and footer template

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
 import "./style.scss";
+import ffsrIcon from "/assets/ffsr.png";
+import Link from "next/link";
 
 export default function RootLayout({
 	children,
@@ -10,7 +13,28 @@ export default function RootLayout({
 			<head />
 			<body>
 				<header>
-
+					<Link
+						href={"/"}
+					>
+						<Image
+							src={ffsrIcon}
+							alt="Accueil"
+						/>
+						<span>
+							Furry Fandom Suisse Romand
+						</span>
+					</Link>
+					<nav>
+						<ul>
+							<li>
+								<Link
+									href={"/"}
+								>
+									Accueil
+								</Link>
+							</li>
+						</ul>
+					</nav>
 				</header>
 
 				<main>
@@ -18,7 +42,18 @@ export default function RootLayout({
 				</main>
 
 				<footer>
-
+					<p>
+						&copy; 2021 Furry Fandom Suisse Romand
+					</p>
+					<p>
+						Designed by <a href="https://dindin.ch">Dindin</a>
+					</p>
+					<p>
+						Build with <a href="https://nextjs.org">Next.js</a>
+					</p>
+					<p>
+						Powered by <a href="https://vercel.com">Vercel</a>
+					</p>
 				</footer>
 			</body>
 		</html>
