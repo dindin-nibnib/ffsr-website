c994ab5ec7fad6d52c0c07a975e3b71048caf15c
Odin Beuchat <odin.beuchat@divtec.ch>
2022-12-29 17:57:03 +0100

    added index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,38 @@
 const Index = () => {
 	return (
 		<>
+			<h1>Furry Fandom Suisse Romand</h1>
 
+			<p>
+				Bienvenue sur la page web du Furry Fandom Suisse Romand (FFSR) !
+			</p>
+
+			<p>
+				Nous sommes une communauté de fans de la culture furry en Suisse romande,
+				réunis par notre passion pour les anthropomorphes.
+				Que vous soyez un artiste, un auteur,
+				un costumier ou simplement quelqu'un qui aime la culture furry,
+				vous êtes le bienvenu dans notre groupe.
+			</p>
+
+			<p>
+				Notre page web est le point de départ pour en savoir plus sur notre communauté
+				et sur les événements que nous organisons régulièrement.
+				Vous trouverez ici des informations sur nos conventions,
+				nos soirées de rencontre et nos ateliers créatifs,
+				ainsi que sur les autres activités auxquelles vous pouvez participer.
+			</p>
+
+			<p>
+				Nous croyons en l'importance de la tolérance,
+				du respect et de l'acceptation de soi,
+				et nous sommes fiers de créer un espace sûr et inclusif pour
+				tous les membres de notre communauté.
+				Si vous êtes intéressé par la culture furry et que vous souhaitez
+				vous impliquer dans notre communauté, n'hésitez pas à nous contacter
+				ou à venir nous rencontrer lors de nos événements.
+				Nous sommes impatients de vous accueillir dans notre famille furry en Suisse romande !
+			</p>
 		</>
 	);
 };
