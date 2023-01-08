b178a7f313390951523f209a8d7366a1927d415e
Odin Beuchat <odin.beuchat@divtec.ch>
2023-01-08 02:11:31 +0100

    added login

--- app/galerie/page.tsx
+++ app/galerie/page.tsx
@@ -1,9 +1,56 @@
 import "./style.scss";
+import "server-only";
+import type Picture from "../../models/gallery";
+import type Account from "../../models/account";
+import { MongoClient, ObjectId } from "mongodb";
+import { use } from "react";
 
 const Gallerie = () => {
+	const client = new MongoClient(process.env.MONGODB_URI || "");
+	const db = client.db("ffsr");
+	const collection = db.collection<Picture>("pictures");
+	const pictures = use(
+		collection.find({}).toArray()
+	);
+
+	const getOwnerName = (owner: ObjectId) => {
+		return use(
+			db.collection<Account>("accounts").findOne({
+				_id: owner
+			})
+		);
+	};
+
 	return (
 		<main className="galerie">
-		</main>
+			<h1>Galerie</h1>
+			<p>
+				Bienvenue sur la galerie du Furry Fandom Suisse Romand!
+			</p>
+
+			<p>
+				Vous pouvez retrouver ici les photos de nos événements.
+				Si vous souhaitez participer à la galerie, n'hésitez pas à nous contacter!
+			</p>
+
+			<div>
+				{
+					pictures.map((picture) => (
+						<picture>
+							<img
+								src={`data:image/png;base64,${picture.data.buffer.toString("base64")}`}
+								alt={picture.name}
+							/>
+							<figcaption>
+								{picture.description} - {
+									getOwnerName(picture.owner)?.displayName
+								}
+							</figcaption>
+						</picture>
+					))
+				}
+			</div>
+		</main >
 	);
 };
 
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,28 +1,44 @@
 "use client";
 
+import { FormEventHandler, use } from "react";
 import "./style.scss";
-import { useRouter } from "next/navigation";
+import Crypto from "crypto";
 
-const login = async (data: any) => {
-	console.log(data);
+const login = async (username: HTMLInputElement | null, password: HTMLInputElement | null) => {
+	let res = await fetch("/api/login", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			username: username?.value,
+			password: Crypto.createHash("sha256").update(password?.value || "").digest("hex")
+		})
+	});
+
+	console.log(res);
+	if (res.ok) {
+		alert("Connexion réussie!");
+		window.location.href = "/galerie";
+	} else {
+		alert(use(res.json()).message || res.statusText || res.status.toString());
+	}
+};
+
+const onSubmit: FormEventHandler = (event) => {
+	event.preventDefault();
+	const children = event.currentTarget.children;
+	login(children.namedItem("username") as HTMLInputElement | null,
+		children.namedItem("password") as HTMLInputElement | null);
 };
 
 const Login = () => {
-	const { push } = useRouter();
 
-	const onSubmit = async (data: any) => {
-		try {
-			await login(data);
-			push('/dashboard');
-		} catch (err) {
-			console.log(err);
-		}
-	};
 
 	return (
 		<main className="login">
 			<form onSubmit={onSubmit}>
-				<input name="email" type="email" />
+				<input name="username" />
 				<input name="password" type="password" />
 				<button type="submit">Login</button>
 			</form>
